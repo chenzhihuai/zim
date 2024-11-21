51194c9a73b6edc3f68940fccb31e9c114dfbc14
chenzhihuai <chenzhihuai@outlook.com>
2024-11-21 22:35:18 +0800

    move lua/*.lua to config

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
-require("options")
-require("keymaps")
-require("autocmds")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
 
 -- dofile(vim.g.base46_cache .. "defaults")
 -- dofile(vim.g.base46_cache .. "statusline")
 
-require("lazy-spec")
+require("config.lazy")
 
 for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
    dofile(vim.g.base46_cache .. v)
